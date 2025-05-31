149963a40e8f1977806892adae81945da374318e
gitsworth <satwiksnaik06@gmail.com>
2025-05-31 20:23:59 +0530

    add main.js

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow, ipcMain, shell } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+// IPC to open external URLs (for flight booking links)
+ipcMain.handle('open-external', async (_, url) => {
+  await shell.openExternal(url);
+});
